08053117b1d7418802c916d9b8fd3bfef17f795b
Selinabernike <selinabernike@gmail.com>
2024-10-26 05:56:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function showResult() {
     scoreElement.innerText = score;
 
     // Ucapan ulang tahun yang mendalam dan menyentuh
-    birthdayMessage.innerText = `Jadi, karena karin sudah berhasil menjawab pertanyaannya, aku ingin memberikan sebuah ucapan special heheng. Happy Birthday, yha karina! On this special day, I want you to know how much you are cherished. Your kindness, your beauty, intelligence, passion and all about you for life inspire those around you. As you step into another year, may it be filled with beautiful moments, new adventures, and dreams that come true. Remember, you are never alone yhaa, i'll be here to support you every step of the way. Celebrate yourself today, because you deserve all the happiness in the world. Here's to another amazing year ahead! just know that youre so important for bernik  🤍🫂`;
+    birthdayMessage.innerText = `Jadi, karena karin sudah berhasil menjawab pertanyaannya, aku ingin memberikan sebuah ucapan special heheng. Happy Birthday, yha karina! On this special day, I want you to know how much you are cherished. Your kindness, your beauty, intelligence, passion and all about you for life inspire those around you especially bernik inii. As you step into another year, may it be filled with beautiful moments, new adventures, and dreams that come true. Remember, you are never alone yhaa, i'll be here to support you every step of the way. Celebrate yourself today, because you deserve all the happiness in the world. Here's to another amazing year ahead! just know that youre so important for bernik  🤍🫂`;
 }
 
 function restartQuiz() {
